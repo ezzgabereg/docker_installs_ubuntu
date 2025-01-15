c370ef6a866526cfbed255e713a1604ce24189ef
Ezz Gaber <79071598+ezzgabereg@users.noreply.github.com>
2025-01-16 00:31:00 +0300

    install_docker_nproxyman.sh

--- install_docker_nproxyman.sh
+++ install_docker_nproxyman.sh
@@ -282,7 +282,7 @@ startInstall()
         mkdir -p docker/nginx-proxy-manager
         cd docker/nginx-proxy-manager
 
-        curl https://gitlab.com/bmcgonag/docker_installs/-/raw/main/docker_compose.nginx_proxy_manager.yml -o docker-compose.yml >> ~/docker-script-install.log 2>&1
+        curl https://gitlab.com/bmcgonag/docker_installs/-/raw/main/docker_compose.nginx_proxy_manager.yml -o docker_compose.nginx_proxy_manager.yml >> ~/docker-script-install.log 2>&1
 
         echo "    2. Running the docker_compose.nginx_proxy_manager.yml to install and start NGinX Proxy Manager"
         echo ""
@@ -360,9 +360,9 @@ startInstall()
         mkdir -p docker/navidrome
         cd docker/navidrome
 
-        curl https://gitlab.com/bmcgonag/docker_installs/-/raw/main/docker_compose_navidrome.yml -o docker-compose.yml >> ~/docker-script-install.log 2>&1
+        curl https://gitlab.com/bmcgonag/docker_installs/-/raw/main/docker_compose_navidrome.yml -o docker_compose_navidrome.yml >> ~/docker-script-install.log 2>&1
 
-        echo "    2. Running the docker-compose.yml to install and start Navidrome"
+        echo "    2. Running the docker_compose_navidrome.yml to install and start Navidrome"
         echo ""
         3echo ""
 
